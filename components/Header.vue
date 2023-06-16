b6462de1e133d1e72cfb13dc90c6799ec178fd4f
justynalodzinska <justyna.lodzinska@smartive.app>
2023-06-16 22:28:45 +0200

    nav elems added

--- components/Header.vue
+++ components/Header.vue
@@ -1,17 +1,48 @@
 <template>
-  <div>
-    <div @click="$router.push('/')">Page title</div>
-    <button @click="$router.push('/cart')">Cart</button>
-    header
-  </div>
+  <nav>
+    <div class="nav-inner">
+<!--      logo-->
+      <div class="logo" @click="$router.push('/')">
+        <img src="~images/nav-logo.jpg" alt="Company logo">
+      </div>
+<!--      links-->
+      <div class="links">
+        <a href="#">About Us</a>
+        <a href="#">Women</a>
+        <a href="#">Men</a>
+        <a href="#">Kids</a>
+        <a href="#">Accesories</a>
+      </div>
+<!--      icons-->
+      <div class="icons">
+        <button @click="$router.push('/cart')">
+
+          <span class="mdi mdi-cart"></span>
+        </button>
+      </div>
+    </div>
+  </nav>
 </template>
 
 <script>
+
+
 export default {
 
 }
 </script>
 
 <style lang="scss" scoped>
-
+nav {
+  height: 100px;
+  .nav-inner {
+    height: 90px;
+    display: flex;
+    justify-content: space-between;
+    align-items: center;
+  }
+  img {
+    height: 50px;
+  }
+}
 </style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,6 +1,11 @@
+import { resolve } from 'path'
+
 export default defineNuxtConfig({
   css: ['@mdi/font/css/materialdesignicons.min.css'],
   modules: [
     "@pinia/nuxt"
-  ]
+  ],
+  alias: {
+    'images': resolve(__dirname, './assets/images'),
+  },
 })
